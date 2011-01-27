845ad0fa0eae9ba4679b7964ba5b456470233a4e
Dustin Heffron <dustin.r.heffron@gmail.com>
2011-01-27 18:45:35 -0500

    Tiki Trap Changes
    Restructured the tiki trap such that it fires projectiles instead of particle effects. Designers can now add a variable amount of weapons to the actual tiki totem and specify when to start shooting a projectile and the cool-down or length of time between each shot. Added a tiki trap to Level 1 as a demonstration.

--- TheScarabGauntlet/Game/Common/Traps/TikiFlameTrapComponent.cs
+++ TheScarabGauntlet/Game/Common/Traps/TikiFlameTrapComponent.cs
@@ -4,32 +4,99 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using GarageGames.Torque.Core;
 using GarageGames.Torque.T2D;
+using GarageGames.Torque.Sim;
 using PlatformerStarter.Common.Util;
 
 namespace PlatformerStarter.Common.Traps
 {
-    [TorqueXmlSchemaType]
-    public class TikiFlameTrapComponent : TrapComponent
+    public class TimedWeapon
     {
         #region Private Members
-        private bool clearParticles;
-        private List<MountedParticle> mountedParticles;
+        private string name;
+        private WeaponComponent weapon;
+        private TimeSpan coolDown;
+        private TimeSpan startTime;
         #endregion
 
         #region Public Properties
-        [TorqueXmlSchemaType(DefaultValue = "1")]
-        public bool ClearFlames
+        
+        /// <summary>
+        /// The name associated with the weapon.
+        /// </summary>
+        public string WeaponName
         {
-            get { return clearParticles; }
-            set { clearParticles = value; }
+            get { return name; }
+            set { name = value; }
         }
 
-        public List<MountedParticle> MountedParticles
+        /// <summary>
+        /// The time to wait between shots.
+        /// </summary>
+        public int CoolDown
         {
-            get { return mountedParticles; }
-            set { mountedParticles = value; }
+            get { return coolDown.Milliseconds; }
+            set { coolDown = TimeSpan.FromMilliseconds(value); }
         }
 
+        /// <summary>
+        /// The time to start the first shot.
+        /// </summary>
+        public int StartTime
+        {
+            get { return startTime.Milliseconds; }
+            set { startTime = TimeSpan.FromMilliseconds(value); }
+        }
+        #endregion
+
+        #region Public Routines
+        /// <summary>
+        /// Initializes all internal members.
+        /// </summary>
+        public void Initialize()
+        {
+            T2DSceneObject weaponObj = TorqueObjectDatabase.Instance.FindObject<T2DSceneObject>(name);
+
+            if (weaponObj != null)
+                weapon = weaponObj.Components.FindComponent<WeaponComponent>();
+        }
+
+        /// <summary>
+        /// Updates the weapon and fires if the cooldown has expired.
+        /// </summary>
+        /// <param name="dt">The change in time between update calls.</param>
+        public void Update(float dt)
+        {
+            startTime = startTime.Subtract(TimeSpan.FromSeconds(dt));
+
+            if (startTime.Seconds <= 0)
+            {
+                weapon.Fire();
+                startTime = coolDown;
+            }
+        }
+        #endregion
+    }
+
+    [TorqueXmlSchemaType]
+    public class TikiFlameTrapComponent : TrapComponent, ITickObject
+    {
+        #region Private Members
+        private bool isActive;
+        private List<TimedWeapon> weapons;
+        #endregion
+
+        #region Public Properties
+        
+        /// <summary>
+        /// A list of names of the mounted weapons on the totem.  This allows the 
+        /// tiki totem to fire each weapon when necessary.
+        /// </summary>
+        public List<TimedWeapon> MountedWeapons
+        {
+            get { return weapons; }
+            set { weapons = value; }
+        }
+        
         public T2DSceneObject SceneObject
         {
             get { return Owner as T2DSceneObject; }
@@ -44,11 +111,29 @@ namespace PlatformerStarter.Common.Traps
         /// </summary>
         public override void Activate()
         {
-            foreach (MountedParticle particle in mountedParticles)
-            {
-                particle.MountTo(SceneObject);
-                particle.Play(clearParticles);
-            }
+            isActive = true;
+        }
+
+        /// <summary>
+        /// Runs the update logic per tick of the game time.
+        /// </summary>
+        /// <param name="move">The exposed movement of the object, if bound.</param>
+        /// <param name="dt">The change in time between ticks.</param>
+        public void ProcessTick(Move move, float dt)
+        {
+            if (isActive)
+                foreach (TimedWeapon weapon in weapons)
+                    weapon.Update(dt);
+        }
+
+        /// <summary>
+        /// Routine to run interpolation logic for component.  Used mainly when the 
+        /// game time is too slow.  Note: This is not implemented for this component.
+        /// </summary>
+        /// <param name="dt">The change in time between ticks.</param>
+        public void InterpolateTick(float dt)
+        {
+            // Move along. Nothing to see here!!
         }
 
         /// <summary>
@@ -58,10 +143,7 @@ namespace PlatformerStarter.Common.Traps
         public override void CopyTo(TorqueComponent obj)
         {
             TikiFlameTrapComponent obj2 = obj as TikiFlameTrapComponent;
-            obj2.ClearFlames = ClearFlames;
-            obj2.MountedParticles = MountedParticles;
-            //obj2.LinkPoints = LinkPoints;
-            //obj2.ParticleTemplate = ParticleTemplate;
+            obj2.MountedWeapons = MountedWeapons;
             
             base.CopyTo(obj);
         }
@@ -80,10 +162,17 @@ namespace PlatformerStarter.Common.Traps
             if (!base._OnRegister(owner) || !(owner is T2DSceneObject))
                 return false;
 
+            foreach (TimedWeapon weapon in weapons)
+                weapon.Initialize();
+            
             // INSERT LOGGER HERE
             // Check particleTemplate
             // Check linkPoints
             //Assert.Fatal(particleTemplate != null, "A particle template must be assigned to the TikiFlameTrapComponent"); 
+            // register for a tick callback
+            ProcessList.Instance.AddTickCallback(SceneObject, this);
+
+            isActive = false;
 
             return true;
         }
@@ -95,8 +184,8 @@ namespace PlatformerStarter.Common.Traps
         {
             //particleTemplate.Dispose();
             //linkPoints.Clear();
-            if(mountedParticles.Count > 0)
-                mountedParticles.Clear();
+            if(weapons.Count > 0)
+                weapons.Clear();
 
             base._OnUnregister();
         }
