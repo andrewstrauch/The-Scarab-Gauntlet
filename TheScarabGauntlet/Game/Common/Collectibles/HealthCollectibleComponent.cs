228e266471288e9c21848b9e3e56181f3e91848c
Dustin Heffron <dustin.r.heffron@gmail.com>
2011-01-29 13:20:31 -0500

    Health Collectible Complete
    Health collectible completely tested and added to test level.

--- TheScarabGauntlet/Game/Common/Collectibles/HealthCollectibleComponent.cs
+++ TheScarabGauntlet/Game/Common/Collectibles/HealthCollectibleComponent.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using GarageGames.Torque.T2D;
+using GarageGames.Torque.Core;
 using GarageGames.Torque.PlatformerFramework;
 
 namespace PlatformerStarter.Common.Collectibles
 {
+    [TorqueXmlSchemaType]
     public class HealthCollectibleComponent : CollectibleComponent
     {
         #region Private Members
@@ -20,11 +22,29 @@ namespace PlatformerStarter.Common.Collectibles
         
         #endregion
 
+        #region Public Routines
+        public override void CopyTo(TorqueComponent obj)
+        {
+            base.CopyTo(obj);
+
+            HealthCollectibleComponent obj2 = obj as HealthCollectibleComponent;
+
+            obj2.HealingValue = HealingValue;
+        }
+        #endregion
         #region Private Routines
         protected override bool _confirmPickup(T2DSceneObject ourObject, T2DSceneObject theirObject, ActorComponent actor)
         {
             if(actor is PlayerActorComponent)
             {
+                if (ourObject.TestObjectType(PlatformerData.SpawnedObjectType))
+                {
+                    CheckpointSystemSpawnedObjectComponent spawnedObject = ourObject.Components.FindComponent<CheckpointSystemSpawnedObjectComponent>();
+
+                    if (spawnedObject != null)
+                        spawnedObject.Recover = false;
+                }
+
                 actor.HealDamage(healingValue, ourObject);
                
                 // true = yes, i was picked up. delete me!
